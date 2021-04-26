1c70493370c593f80f7c55fc37a82694e44c5f64
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-04-26 07:25:44 -0300

    boolean - aulas 9 e 10

---
+++ modulo-02/10-aula-tipo-boolean/booleanType.js
@@ -0,0 +1,14 @@
+"use strict";
+// => Exemplo 1 
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+// => Exemplo 2
+let concluido = false;
+if (!concluido) {
+    console.log('A tarefa foi concluída com sucesso!!');
+}
+else {
+    console.log('A tarefa esta pendente!!');
+}
---
+++ modulo-02/10-aula-tipo-boolean/booleanType.ts
@@ -0,0 +1,17 @@
+// => Exemplo 1 
+
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+// => Exemplo 2
+
+let concluido: boolean = false;
+
+if (!concluido) {
+    console.log('A tarefa foi concluída com sucesso!!');
+} else {
+    console.log('A tarefa esta pendente!!');
+}
