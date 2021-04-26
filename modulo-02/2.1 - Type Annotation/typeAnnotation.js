6dee12c14f7650c60d921cea9f3edddf0f964ee0
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-04-26 07:13:02 -0300

    typeAnnotation - aulas 7 e 8

---
+++ modulo-02/2.1 - Type Annotation/typeAnnotation.js
@@ -0,0 +1,16 @@
+"use strict";
+// => Variáveis [Type Annotations]
+let nome = 'Christian Oliveira';
+console.log(nome);
+// => Arrays [Type Annotations]
+let animais = ['Elefante', 'Cachorro', 'Gato', 'Panda', 'Girafa'];
+console.log(animais);
+// => Objetos [Type Annotations]
+let carro;
+carro = { nome: 'Toyota Yaris Sedan XS', ano: 2019, preco: 80000 };
+console.log(carro);
+// => Functions [Type Annotations]
+function multiplicaNumero(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicaNumero(2, 5));
---
+++ modulo-02/2.1 - Type Annotation/typeAnnotation.ts
@@ -0,0 +1,25 @@
+// => Variáveis [Type Annotations]
+let nome: string = 'Christian Oliveira';
+console.log(nome);
+
+// => Arrays [Type Annotations]
+let animais: string[] = ['Elefante', 'Cachorro', 'Gato', 'Panda', 'Girafa'];
+console.log(animais);
+
+// => Objetos [Type Annotations]
+let carro: {
+    nome: string;
+    ano: number;
+    preco: number;
+};
+
+carro = { nome: 'Toyota Yaris Sedan XS', ano: 2019, preco: 80000 }
+console.log(carro);
+
+// => Functions [Type Annotations]
+
+function multiplicaNumero(num1: number, num2: number) {
+    return num1 * num2;
+}
+
+console.log(multiplicaNumero(2,5));
